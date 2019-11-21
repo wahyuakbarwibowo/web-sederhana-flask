cefd6b74d47209af5188ea091085c3d9bab6d2cc
Wahyu Akbar <wahyuakbarwibowo@gmail.com>
2019-11-21 11:22:11 +0700

    change convert.html

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from PIL import Image
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return render_template('index.html
+    return render_template('index.html')
 
 @app.route('/image')
 def load():
